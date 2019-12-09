5aa97beb2a166c57258ac17e864209379d4c468a
Vilson Blanco Dauinheimer <Bwolfnoob@gmail.com>
2019-12-08 22:50:01 -0300

    clean test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:example/app/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
