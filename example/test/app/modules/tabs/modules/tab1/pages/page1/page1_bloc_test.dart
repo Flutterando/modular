5d85d0985d15fd51afbe1df1d75b6f1c04f3067e
Bwolfs2 <bwolfnoob@gmail.com>
2020-03-24 22:32:48 -0300

    remove warnings

--- example/test/app/modules/tabs/modules/tab1/pages/page1/page1_bloc_test.dart
+++ example/test/app/modules/tabs/modules/tab1/pages/page1/page1_bloc_test.dart
@@ -1,14 +1,11 @@
-import 'package:flutter_modular/flutter_modular_test.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:example/app/app_module.dart';
-import 'package:example/app/modules/tabs/modules/tab1/pages/page1/page1_bloc.dart';
 import 'package:example/app/modules/tabs/modules/tab1/tab1_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
   Modular.init(AppModule());
   Modular.bindModule(Tab1Module());
-  Page1Bloc bloc;
+  //Page1Bloc bloc;
 
   // setUp(() {
   //     bloc = Tab1Module.to.get<Page1Bloc>();
--- example/test/app/modules/tabs/modules/tab1/tab1_bloc_test.dart
+++ example/test/app/modules/tabs/modules/tab1/tab1_bloc_test.dart
@@ -1,14 +1,11 @@
-import 'package:flutter_modular/flutter_modular_test.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:example/app/app_module.dart';
-import 'package:example/app/modules/tabs/modules/tab1/tab1_bloc.dart';
 import 'package:example/app/modules/tabs/modules/tab1/tab1_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
   Modular.init(AppModule());
   Modular.bindModule(Tab1Module());
-  Tab1Bloc bloc;
+  //Tab1Bloc bloc;
 
   // setUp(() {
   //     bloc = Tab1Module.to.get<Tab1Bloc>();
