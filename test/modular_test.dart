5d7bca6c8737699b9e858241433b8831095be428
allan <luciocorreia.allan@gmail.com>
2020-05-28 17:25:13 -0300

    Add a Modular test interface

--- test/modular_test.dart
+++ test/modular_test.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_modular/flutter_modular.dart';
-
 import 'app/app_module.dart';
 import 'app/modules/product/product_module.dart';
 
