16cc6ea5288b1dae8cea83ca17d8dd2e28640c14
Jacob  Moura <jacobmoura@iMac-Pro.local>
2020-11-03 15:14:36 -0300

    added new constructors

--- flutter_modular/lib/src/inject/bind.dart
+++ flutter_modular/lib/src/inject/bind.dart
@@ -13,6 +13,25 @@ class Bind<T> {
   Bind(this.inject, {this.singleton = true, this.lazy = true})
       : assert((singleton || lazy),
             r"'singleton' can't be false if 'lazy' is also false");
+
+  ///Bind a 'Singleton' class.
+  ///Built together with the module.
+  ///The instance will always be the same.
+  factory Bind.singleton(T Function(Inject i) inject) {
+    return Bind(inject, singleton: true, lazy: false);
+  }
+
+  ///Bind a 'Lazy Singleton' class.
+  ///Built only when called the first time using Modular.get.
+  ///The instance will always be the same.
+  factory Bind.lazySingleton(T Function(Inject i) inject) {
+    return Bind(inject, singleton: true, lazy: true);
+  }
+
+  ///Bind a factory. Always a new constructor when calling Modular.get
+  factory Bind.factory(T Function(Inject i) inject) {
+    return Bind(inject, singleton: false, lazy: false);
+  }
 }
 
 class BindInject<T> extends Bind<T> {
