dcf0d6c507b355e617686e9e2151caf7ee5c401e
Jacob Moura <jacobaraujo7@gmail.com>
2021-04-10 23:21:27 -0300

    fix #359

--- flutter_modular/lib/src/core/models/child_route.dart
+++ flutter_modular/lib/src/core/models/child_route.dart
@@ -6,7 +6,7 @@ import '../interfaces/route_guard.dart';
 import 'custom_transition.dart';
 import 'modular_arguments.dart';
 
-class ChildRoute extends ModularRouteImpl {
+class ChildRoute<T> extends ModularRouteImpl<T> {
   ChildRoute(
     String routerName, {
     List<ModularRoute> children = const [],
@@ -14,7 +14,7 @@ class ChildRoute extends ModularRouteImpl {
     List<RouteGuard>? guards,
     TransitionType transition = TransitionType.defaultTransition,
     CustomTransition? customTransition,
-    RouteBuilder? routeGenerator,
+    RouteBuilder<T>? routeGenerator,
     Duration duration = const Duration(milliseconds: 300),
   }) : super(routerName,
             routerOutlet: [], duration: duration, child: child, routeGenerator: routeGenerator, customTransition: customTransition, children: children, guards: guards, transition: transition);
