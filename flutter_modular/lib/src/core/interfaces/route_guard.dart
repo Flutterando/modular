a54dcabfd5acd8673bed41790c612d3eaf7c508f
Magomed Dadagov <magomed.dadagov@orioninc.com>
2021-05-04 15:04:15 +0300

    move guardedRoute ModularRoute->RouteGuard

--- flutter_modular/lib/src/core/interfaces/route_guard.dart
+++ flutter_modular/lib/src/core/interfaces/route_guard.dart
@@ -4,5 +4,9 @@ import 'modular_route.dart';
 
 // ignore: one_member_abstracts
 abstract class RouteGuard {
+  RouteGuard(this.guardedRoute);
+
+  final String? guardedRoute;
+
   Future<bool> canActivate(String path, ModularRoute router);
 }
