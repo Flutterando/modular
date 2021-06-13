69bd52d2efd8904b8c595b709ce525db172298a5
Ascênio Sanderson <ascenio@hotmail.com>
2021-06-13 17:02:00 -0300

    refactor: deletes Bind's type checking test

--- flutter_modular/test/src/core/models/bind_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_modular/src/core/models/bind.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  test('check type', () {
-    final bind = Bind.instance('teste');
-    expect(bind, isA<Bind<String>>());
-  });
-}
