8b7074bb2816d199883bf87d0864cfd671ad6204
Ascênio Sanderson <ascenio@hotmail.com>
2021-06-13 18:20:37 -0300

    refactor: move bind test to a group

--- flutter_modular/test/src/core/inject/bind_test.dart
+++ flutter_modular/test/src/core/inject/bind_test.dart
@@ -3,19 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Bind', () {
-    test("can't be both singleton and lazy", () {
-      expect(
-        () => Bind((i) => String, isLazy: false, isSingleton: false),
-        throwsAssertionError,
-      );
-    });
-
     group('factories', () {
       test('defaults to lazy and singleton', () {
         final bind = Bind((_) => 'some dependency');
         expect(bind.isSingleton, isTrue);
         expect(bind.isLazy, isTrue);
         expect(bind.export, isFalse);
+      });
+
+      test("can't be both singleton and lazy", () {
+        expect(
+          () => Bind((i) => String, isLazy: false, isSingleton: false),
+          throwsAssertionError,
+        );
       });
     });
   });
