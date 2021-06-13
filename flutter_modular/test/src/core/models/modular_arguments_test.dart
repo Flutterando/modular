c994771d0d6c9557155d719dd6127d736ae02539
Ascênio Sanderson <ascenio@hotmail.com>
2021-06-13 17:06:33 -0300

    refactor: improves ModularArguments' copyWith test

--- flutter_modular/test/src/core/models/modular_arguments_test.dart
+++ flutter_modular/test/src/core/models/modular_arguments_test.dart
@@ -3,8 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 main() {
   test('should make copy with implementation', () {
-    final model = ModularArguments.empty();
+    final model =
+        ModularArguments(data: 'data', params: {}, uri: Uri.parse("/route"));
     final copy = model.copyWith();
-    expect(model != copy, true);
+    expect(model, isNot(equals(copy)));
+    expect(model.params, copy.params);
+    expect(model.data, copy.data);
+    expect(model.uri, copy.uri);
   });
 }
