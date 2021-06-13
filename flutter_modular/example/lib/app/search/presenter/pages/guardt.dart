a094fb8f60cbffea44f418b40450c3540eee493e
Ascênio Sanderson <ascenio@hotmail.com>
2021-06-13 16:42:24 -0300

    fix: adds constructor to GuardT

--- flutter_modular/example/lib/app/search/presenter/pages/guardt.dart
+++ flutter_modular/example/lib/app/search/presenter/pages/guardt.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
 class GuardT extends RouteGuard {
+  GuardT(String guardedRoute) : super(guardedRoute);
+
   @override
   Future<bool> canActivate(String path, ModularRoute router) async {
     print(Modular.args);
