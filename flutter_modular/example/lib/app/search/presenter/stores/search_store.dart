75117a586c5a04a8a3b4f79454caad9dd630ed31
Jacob Moura <jacobaraujo7@gmail.com>
2021-08-02 23:37:04 -0300

    fix example

--- flutter_modular/example/lib/app/search/presenter/stores/search_store.dart
+++ flutter_modular/example/lib/app/search/presenter/stores/search_store.dart
@@ -1,15 +1,14 @@
-import 'package:dartz/dartz.dart';
 import 'package:example/app/core/dartz_adapter/dartz_adapter.dart';
 import 'package:example/app/search/domain/entities/result.dart';
 import 'package:example/app/search/domain/errors/erros.dart';
 import 'package:example/app/search/domain/usecases/search_by_text.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:mobx_triple/mobx_triple.dart';
+import 'package:flutter_triple/flutter_triple.dart';
 
 part 'search_store.g.dart';
 
 @Injectable()
-class SearchStore extends MobXStore<Failure, List<Result>> {
+class SearchStore extends NotifierStore<Failure, List<Result>> {
   final SearchByText searchByText;
 
   SearchStore(this.searchByText) : super([]);
