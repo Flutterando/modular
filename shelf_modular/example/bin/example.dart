18631b64136aac1482ecc6873c017aa71de4b916
Jacob Moura <jacobaraujo7@gmail.com>
2021-08-29 03:11:58 -0300

    remove package

--- shelf_modular/example/bin/example.dart
+++ shelf_modular/example/bin/example.dart
@@ -2,14 +2,8 @@ import 'package:example/src/app_module.dart';
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_modular/shelf_modular.dart';
-import 'package:shelf_web_socket/shelf_web_socket.dart';
 
 void main(List<String> args) async {
-  var handlerWebSocket = webSocketHandler((webSocket) {
-    webSocket.stream.listen((message) {
-      webSocket.sink.add('echo $message');
-    });
-  });
   final handler = shelf.Pipeline().addMiddleware(shelf.logRequests()).addHandler(Modular(module: AppModule()));
   // final handler = shelf.Pipeline().addMiddleware(shelf.logRequests()).addHandler(Modular(module: AppModule()));
 
