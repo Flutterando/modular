3884eacc74898a0e6c6209bf4d9c9b1e62bd2d59
Jacob Moura <jacobaraujo7@gmail.com>
2021-09-05 18:29:21 -0300

    add test

---
+++ flutter_modular/test/src/presenter/guards/route_guard_test.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular/src/presenter/models/route.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:modular_core/modular_core.dart';
+
+void main() {
+  test('instance', () async {
+    final route = ChildRoute('/', child: (_, __) => Container());
+    final redirect = await MyGuard().pos(route, ModularArguments.empty());
+    expect(redirect, isA<RedirectRoute>());
+  });
+}
+
+class MyGuard extends RouteGuard {
+  MyGuard() : super('/');
+
+  @override
+  FutureOr<bool> canActivate(String path, ParallelRoute route) {
+    return false;
+  }
+}
