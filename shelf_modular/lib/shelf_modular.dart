00e5e91610fd22dd82ebd9ef22ad5fc3b3a98a38
Jacob Moura <jacobaraujo7@gmail.com>
2022-06-03 10:50:35 -0300

    fix: remove imports

--- shelf_modular/lib/shelf_modular.dart
+++ shelf_modular/lib/shelf_modular.dart
@@ -4,14 +4,13 @@ import 'package:shelf_modular/src/shelf_modular_module.dart';
 
 import 'src/presenter/modular_base.dart';
 
-export 'src/presenter/models/route.dart';
+export 'package:modular_core/modular_core.dart' show ModularRoute, ModularArguments, Injector, Disposable;
+
 export 'src/presenter/models/bind.dart';
-export 'src/presenter/guards/route_guard.dart';
 export 'src/presenter/models/module.dart';
-export 'src/presenter/resources/websocket_resource.dart';
+export 'src/presenter/models/route.dart';
 export 'src/presenter/resources/resource.dart';
-export 'package:modular_core/modular_core.dart'
-    show ModularRoute, ModularArguments, Injector, Disposable;
+export 'src/presenter/resources/websocket_resource.dart';
 
 // ignore: non_constant_identifier_names
 final Modular = injector<IModularBase>();
