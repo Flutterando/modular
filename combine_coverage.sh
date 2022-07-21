4823c44f4f58dab0b09e691e5e8988ad7834b445
Bwolf <bwolfnoob@gmail.com>
2022-07-21 19:15:12 -0300

    Fix combine coverage

--- combine_coverage.sh
+++ combine_coverage.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-escapedPath="$(echo pwd | sed 's///\//g')"
+escapedPath="$(echo `pwd` | sed 's/\//\\\//g')"
 
 if grep flutter pubspec.yaml > /dev/null; then
   if [ -d "coverage" ]; then
     # combine line coverage info from package tests to a common file
     if [ ! -d "$MELOS_ROOT_PATH/coverage_report" ]; then
       mkdir "$MELOS_ROOT_PATH/coverage_report"
+      ls "$MELOS_ROOT_PATH/coverage_report"
+      ls "$MELOS_ROOT_PATH"
     fi
-    sed "s/^SF:lib/SF:$escapedPath/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage_report/lcov.info"
+    sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage_report/lcov.info"
     rm -rf "coverage"
   fi
 fi
