fdfb059f0243d2c048182ba122a6e20b57ad840a
Alvaro Vasconcelos <sr.vasconcelos01@gmail.com>
2022-07-29 06:25:53 -0300

    fix: fix coverage combine in windows

--- scripts/combine_coverage.sh
+++ scripts/combine_coverage.sh
@@ -5,12 +5,12 @@ escapedPath="$(echo `pwd` | sed 's/\//\\\//g')"
 if grep flutter pubspec.yaml > /dev/null; then
   if [ -d "coverage" ]; then
     # combine line coverage info from package tests to a common file
-    if [ ! -d "$MELOS_ROOT_PATH/coverage_report" ]; then
-      mkdir "$MELOS_ROOT_PATH/coverage_report"
-      ls "$MELOS_ROOT_PATH/coverage_report"
+    if [ ! -d "$MELOS_ROOT_PATH/coverage" ]; then
+      mkdir "$MELOS_ROOT_PATH/coverage"
+      ls "$MELOS_ROOT_PATH/coverage"
       ls "$MELOS_ROOT_PATH"
     fi
-    sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage_report/lcov.info"
+    sed "s/^SF:lib/SF:$escapedPath\/lib/g" coverage/lcov.info >> "$MELOS_ROOT_PATH/coverage/lcov.info"
     rm -rf "coverage"
   fi
 fi
