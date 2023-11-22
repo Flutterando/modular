81e14b4f82d082ced3fdbf8176f24bf38627c348
vatsal tanna <vatsal.t@simformsolutions.com>
2023-11-22 22:56:21 +0530

    Fix Disposable is not a mixin class

--- modular_core/lib/src/module/disposable.dart
+++ modular_core/lib/src/module/disposable.dart
@@ -3,7 +3,7 @@ part of '../../modular_core.dart';
 /// A class which implements [Disposable] can be disposed automatically
 /// once user leaves a Module
 //ignore:one_member_abstracts
-abstract class Disposable {
+mixin abstract class Disposable {
   /// Disposes controllers, streams, etc.
   void dispose();
 }
